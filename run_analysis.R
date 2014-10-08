295bfc9a2e422cc80f1f40ed99cad6169aa28a3e
bschneeman <bschneeman@homeaway.com>
2014-10-08 16:00:52 -0500

    some refactoring/re-organizing and writing the final file

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
 # Analysis for Coursera Getting and Cleaning Data Programming Assignment
-#
+
+# I'm not super familiar with data.table, so I'm going to try it. Hopefully this
+# won't end in tears
 require(data.table)
 require(dplyr)
 require(tidyr)
@@ -29,14 +31,17 @@ subjects = rbind(subjectTrain, subjectTest)
 #activity files
 activityTrain = fread(file.path(dataDir, "train", "Y_train.txt"))
 activityTest  = fread(file.path(dataDir, "test" , "Y_test.txt" ))
-## merge them
-activityCodes = rbind(activityTrain, activityTest)
 
 #meh fread is not happy with the X_train.txt files, so use read.table
 dataTrain = data.table(read.table(file.path(dataDir, "train", "X_train.txt")))
 dataTest = data.table(read.table(file.path(dataDir, "test", "X_test.txt")))
 
-## merge them
+# and read in the features
+features = fread(file.path(dataDir, "features.txt"))
+setnames(features, names(features), c("featureNumber", "featureName"))
+
+## merge the data tables together
+activityCodes = rbind(activityTrain, activityTest)
 activityData = rbind(dataTrain, dataTest)
 
 # little clean up
@@ -49,11 +54,13 @@ setnames(activityCodes, "V1", "activityNumber")
 # and one more binding
 activityData = cbind(subjects, activityCodes, activityData)
 
+# pull in the activity names, too
+activityLabels = fread(file.path(dataDir, "activity_labels.txt"))
+setnames(activityLabels, names(activityLabels), c("activityNumber", "activityLabel"))
+
 # now we'll want to extract the measures of interest. These are identified by
 # feature names containing "mean" and "std" (for standard deviation)
 
-features = fread(file.path(dataDir, "features.txt"))
-setnames(features, names(features), c("featureNumber", "featureName"))
 
 features = features %>% filter(grepl(".*-std.*|.*-mean.*", featureName))
 
@@ -61,11 +68,27 @@ features$featureCode = features[, paste0("V", featureNumber)]
 
 keeps = c("subjectNumber", "activityNumber", features$featureCode)
 
+
 # not sure why I have to do this, but the subset won't work otherwise...
 # (something with data.table that I'm not familiar with)
 setkey(activityData, subjectNumber, activityNumber)
 
 activityData = activityData[,keeps, with=FALSE]
 
-setnames(activityData, names(activityData), c("subjectNumber", "activityNumber", features$featureName))
+setnames(activityData, names(activityData), 
+         c("subjectNumber", "activityNumber", features$featureName))
 
+activityData = merge(activityData, activityLabels, by="activityNumber")
+
+# when I merged, my last column is now the Activity Label I want to only 
+# calculate stats on the features, so I need to index into 'gather' nicely
+lastDataColumn = ncol(activityData)-1
+
+activityDataTidy = gather(activityData, featureName, featureMeasure, 3:lastDataColumn)
+
+# create the tidy summary data set. Note, long and narrow
+summaryActivityDataTidy = activityDataTidy %>% 
+  group_by(subjectNumber, activityLabel, featureName) %>% 
+  summarize(mean=mean(featureMeasure), sd=sd(featureMeasure))
+
+write.table(summaryActivityDataTidy, "summaryActivityData.txt", row.names=F)
