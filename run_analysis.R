9d73920c3586d02d0576093a52023dff51e3fcea
bschneeman <bschneeman@homeaway.com>
2014-10-08 10:51:11 -0500

    bound all data into one big datatable

--- run_analysis.R
+++ run_analysis.R
@@ -19,3 +19,31 @@ if(!file.exists(dataDir)){
 
 # at this point, we can be reasonably assured that the raw data is around, so read it in
 
+#subject files
+subjectTrain = fread(file.path(dataDir, "train", "subject_train.txt"))
+subjectTest  = fread(file.path(dataDir, "test" , "subject_test.txt" ))
+## merge them
+subjects = rbind(subjectTrain, subjectTest)
+
+#activity files
+activityTrain = fread(file.path(dataDir, "train", "Y_train.txt"))
+activityTest  = fread(file.path(dataDir, "test" , "Y_test.txt" ))
+## merge them
+activityLabels = rbind(activityTrain, activityTest)
+
+#meh fread is not happy with the X_train.txt files, so use read.table
+dataTrain = data.table(read.table(file.path(dataDir, "train", "X_train.txt")))
+dataTest = data.table(read.table(file.path(dataDir, "test", "X_test.txt")))
+
+## merge them
+activityData = rbind(dataTrain, dataTest)
+
+# little clean up
+#rm(subjectTrain, subjectTest, activityTrain, activityTest, dataTrain, dataTest)
+
+# add some nice labels
+setnames(subjects,"V1", "subjectNumber")
+setnames(activityLabels, "V1", "activityNumber")
+
+# and one more binding
+activityData = cbind(subjects, activityLabels, activityData)
